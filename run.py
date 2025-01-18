80e3f5cd7174131f01ddb543bbb8e0ef60b67bd8
João Victor <jvcgomes14@gmail.com>
2025-01-18 12:08:27 -0300

    organized a new structure for the project

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
+from segmentation.thresholding.fixed_threshold import fixed_threshold
+from segmentation.thresholding.otsu_threshold import otsu_threshold
+from segmentation.thresholding.adaptative_threshold import adaptive_threshold
+
+# Image path
+image_path = 'image.jpg'
+
+# Call each function
+print("Applying Fixed Threshold...")
+fixed_threshold(image_path)
+
+print("Applying Adaptive Threshold...")
+adaptive_threshold(image_path)
+
+print("Applying Otsu Threshold...")
+otsu_threshold(image_path)
+
+################################################# MUDAR EM BAIXO
+
 from flask import Flask, flash, request, redirect, url_for, render_template
 import urllib.request
 import os
