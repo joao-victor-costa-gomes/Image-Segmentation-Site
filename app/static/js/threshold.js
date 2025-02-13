fff90621f7a74fe65f4cd081d74d1ad4317da218
João Victor <jvcgomes14@gmail.com>
2025-02-13 11:20:54 -0300

    automatic image send

--- app/static/js/threshold.js
+++ app/static/js/threshold.js
@@ -1,5 +1,6 @@
 const dropArea = document.getElementById('drop-area');
 const fileInput = document.getElementById('file-input');
+const uploadForm = document.getElementById('upload-form');
 
 // Evita o comportamento padrão para eventos de arrastar e soltar
 ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(event => {
@@ -20,9 +21,17 @@ dropArea.addEventListener('dragleave', () => {
 dropArea.addEventListener('drop', (e) => {
     dropArea.classList.remove('dragover');
 
-    // Obtém o arquivo solto
+    // Obtém o arquivo solto e atribui ao input
     const files = e.dataTransfer.files;
     if (files.length > 0) {
         fileInput.files = files;
+        uploadForm.submit(); // Envia o formulário automaticamente
+    }
+});
+
+// Envia automaticamente ao selecionar o arquivo via clique
+fileInput.addEventListener('change', () => {
+    if (fileInput.files.length > 0) {
+        uploadForm.submit();
     }
 });
